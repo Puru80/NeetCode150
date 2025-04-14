5fa884f05a7e8524a23fa2aad97162a8350d2f37
Puru80 <puru.agar99@gmail.com>
2025-04-14 11:14:55 +0530

    1 Problem

---
+++ ValidParenttheses.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+
+public class ValidParenttheses {
+
+    public static PrintWriter pw;
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() throws FileNotFoundException {
+            br = new BufferedReader(
+                    new FileReader("D:/Interview Prep/neetcode150/input.txt"));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (Character c : s.toCharArray()) {
+            if (stack.isEmpty() || c == '[' || c == '{' || c == '(') {
+                stack.push(c);
+            } else if(c == '}' && stack.peek() == '{'){
+                stack.pop();
+            } else if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastReader input = new FastReader();
+        pw = new PrintWriter(new BufferedWriter(new FileWriter("D:/Interview Prep/neetcode150/output.txt")));
+        int t = input.nextInt();
+
+        while (t-- > 0) {
+            String s = input.nextLine();
+
+            pw.println(isValid(s));
+        }
+
+        pw.flush();
+    }
+}
