d2405e9b0e3fc99b7aa494edf1b419803a3e1bba
Puru80 <puru.agar99@gmail.com>
2025-05-02 11:40:15 +0530

    1 Problem

---
+++ TimeBasedKVStore.java
@@ -0,0 +1,114 @@
+import java.util.*;
+import java.io.*;
+
+class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
+
+class TimeMap {
+
+    private Map<String, List<Pair<Integer, String>>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, k -> new ArrayList<>())
+                .add(new Pair<>(timestamp, value));
+    }
+
+    public String get(String key, int timestamp) {
+        List<Pair<Integer, String>> list = map.getOrDefault(key, new ArrayList<>());
+        int l = 0, r = list.size() - 1;
+        String res = "";
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int curTimeStamp = list.get(mid).getKey();
+
+            if(curTimeStamp > timestamp){
+                r = mid - 1;
+            } else{
+                res = list.get(mid).getValue();
+                l = mid + 1;
+            }
+        }
+
+        return res;
+    }
+}
+
+public class TimeBasedKVStore {
+
+    public static PrintWriter pw;
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() throws FileNotFoundException {
+            br = new BufferedReader(
+                    new FileReader("D:/Interview Prep/neetcode150/input.txt"));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastReader input = new FastReader();
+        pw = new PrintWriter(new BufferedWriter(new FileWriter("D:/Interview Prep/neetcode150/output.txt")));
+        int t = input.nextInt();
+
+        while (t-- > 0) {
+
+        }
+
+        pw.flush();
+    }
+}
