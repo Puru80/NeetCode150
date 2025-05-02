9401102b09ecec72d727a3d214f63c656498a68f
Puru80 <puru.agar99@gmail.com>
2025-05-02 10:50:19 +0530

    1 Problem

---
+++ MinRotSortedArray.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+public class MinRotSortedArray {
+
+    public static PrintWriter pw;
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() throws FileNotFoundException {
+            br = new BufferedReader(
+                    new FileReader("D:/Interview Prep/neetcode150/input.txt"));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static int findMin(int[] nums){
+        int l=0, r = nums.length-1;
+
+        while(l < r){
+            int mid = l + (r - l)/2;
+            if(nums[r] > nums[mid]){
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return nums[l];
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastReader input = new FastReader();
+        pw = new PrintWriter(new BufferedWriter(new FileWriter("D:/Interview Prep/neetcode150/output.txt")));
+        int t = input.nextInt();
+
+        while (t-- > 0) {
+            int[] nums = Arrays.stream(input.nextLine().split(" ")).mapToInt(s -> Integer.parseInt(s))
+                    .toArray();
+            pw.println(findMin(nums));
+        }
+
+        pw.flush();
+    }
+}
