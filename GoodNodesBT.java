44419285a807b64a44c3e3ef89a11798ffd43fe2
Puru80 <puru.agar99@gmail.com>
2025-05-06 23:13:05 +0530

    1 Problem

---
+++ GoodNodesBT.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+public class GoodNodesBT {
+
+    public static PrintWriter pw;
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() throws FileNotFoundException {
+            br = new BufferedReader(
+                    new FileReader("D:/Interview Prep/neetcode150/input.txt"));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    private int dfs(TreeNode root, int maxVal) {
+        if (root == null) {
+            return 0;
+        }
+
+        int res = root.val >= maxVal ? 1 : 0;
+
+        res += dfs(root.left, Math.max(maxVal, root.val));
+        res += dfs(root.right, Math.max(maxVal, root.val));
+
+        return res;
+    }
+
+    public int goodNodes(TreeNode root) {
+        return dfs(root, root.val);
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastReader input = new FastReader();
+        pw = new PrintWriter(new BufferedWriter(new FileWriter("D:/Interview Prep/neetcode150/output.txt")));
+        int t = input.nextInt();
+
+        while (t-- > 0) {
+
+        }
+
+        pw.flush();
+    }
+}
