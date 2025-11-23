c860bd12f0a7e2095d07bc7eb3282de392e22bfd
Puru80 <puru.agar99@gmail.com>
2025-11-23 11:44:50 +0530

    New Sys commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 2; j++)
+                System.out.print(i * j);
+        }
+    }
+    
+}
