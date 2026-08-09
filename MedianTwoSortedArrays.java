6344500020e8e4f2ff903607a057b5be13a01ccc
Puru80 <puru.agar99@gmail.com>
2026-08-09 19:13:43 +0530

    Median of two sorted arrays

--- MedianTwoSortedArrays.java
+++ MedianTwoSortedArrays.java
@@ -48,41 +48,41 @@ public class MedianTwoSortedArrays {
         }
     }
 
-    /*
-     * public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-     * int n = nums1.length, m = nums2.length;
-     * 
-     * int mid = (n + m) / 2;
-     * 
-     * int i = 0, j = 0, m1 = 0, m2 = 0;
-     * for (int k = 0; k <= mid; k++) {
-     * m2 = m1;
-     * if (i != n && j != m) {
-     * if (nums1[i] >= nums2[j]) {
-     * m1 = nums2[j++];
-     * } else {
-     * m1 = nums1[i++];
-     * }
-     * } else if (i < n) {
-     * m1 = nums1[i++];
-     * } else {
-     * m1 = nums2[j++];
-     * }
-     * }
-     * 
-     * if ((n + m) % 2 == 0) {
-     * double ans = (double) m1 + (double) m2;
-     * return ans / 2;
-     * } else {
-     * return (double) m1;
-     * }
-     * }
-     */
-
+    
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+
+        int mid = (n + m) / 2;
+
+        int i=0, j=0, m1=0, m2=0;
+        for(int k=0; k<=mid; k++){
+            m2=m1;
+            if(i != n && j != m){
+                if(nums1[i] >= nums2[j]){
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
+                }
+            } else if(i < n){
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
+            }
+        }
+
         
-        return 0;
+
+        if((n+m) % 2 == 0){
+            double ans = (double)m1 + (double)m2;
+            return ans/2;
+        } else return (double)m1;
     }
+    
+
+    // public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+    //     return 0;
+    // }
 
     public static void main(String[] args) throws Exception {
         FastReader input = new FastReader();
